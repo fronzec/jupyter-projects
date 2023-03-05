1b206c77ee9b991182c0416f90e4ce95cc364d56
Eduardo Flores <efloresambrosio@gmail.com>
2023-03-05 00:25:45 -0600

    wip: better alternative for plotting

--- ploting/_devenvironment/db/data/00-ranges-example.sql
+++ ploting/_devenvironment/db/data/00-ranges-example.sql
@@ -1,116 +1,22 @@
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ccc', 500, 1000, 500+46, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ccc', 500, 1000, 500+76, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ccc', 500, 1000, 500+123, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ccc', 500, 1000, 500+128, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ccc', 500, 1000, 500+245, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ccc', 500, 1000, 500+555, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ccc', 500, 1000, 500+688, '2022-07-05');
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ddd', 500, 1000, 500+4, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ddd', 500, 1000, 500+55, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ddd', 500, 1000, 500+99, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ddd', 500, 1000, 500+122, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ddd', 500, 1000, 500+197, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ddd', 500, 1000, 500+211, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ddd', 500, 1000, 500+217, '2022-07-05');
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'eee', 500, 1000, 500+10, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'eee', 500, 1000, 500+16, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'eee', 500, 1000, 500+23, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'eee', 500, 1000, 500+28, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'eee', 500, 1000, 500+45, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'eee', 500, 1000, 500+55, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'eee', 500, 1000, 500+88, '2022-07-05');
-
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'fff', 500, 1000, 500+11, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'fff', 500, 1000, 500+18, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'fff', 500, 1000, 500+25, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'fff', 500, 1000, 500+32, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'fff', 500, 1000, 500+48, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'fff', 500, 1000, 500+59, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'fff', 500, 1000, 500+99, '2022-07-05');
-
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ggg', 500, 1000, 500+15, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ggg', 500, 1000, 500+25, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ggg', 500, 1000, 500+35, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ggg', 500, 1000, 500+45, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ggg', 500, 1000, 500+55, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ggg', 500, 1000, 500+65, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ggg', 500, 1000, 500+77, '2022-07-05');
-
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'hhh', 500, 1000, 500+10, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'hhh', 500, 1000, 500+16, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'hhh', 500, 1000, 500+23, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'hhh', 500, 1000, 500+28, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'hhh', 500, 1000, 500+45, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'hhh', 500, 1000, 500+55, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'hhh', 500, 1000, 500+88, '2022-07-05');
-
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'iii', 500, 1000, 500+10, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'iii', 500, 1000, 500+16, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'iii', 500, 1000, 500+23, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'iii', 500, 1000, 500+28, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'iii', 500, 1000, 500+45, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'iii', 500, 1000, 500+55, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'iii', 500, 1000, 500+88, '2022-07-05');
-
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'jjj', 500, 1000, 500+10, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'jjj', 500, 1000, 500+27, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'jjj', 500, 1000, 500+35, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'jjj', 500, 1000, 500+46, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'jjj', 500, 1000, 500+58, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'jjj', 500, 1000, 500+88, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'jjj', 500, 1000, 500+100, '2022-07-05');
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'kkk', 500, 1000, 500+110, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'kkk', 500, 1000, 500+110, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'kkk', 500, 1000, 500+120, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'kkk', 500, 1000, 500+120, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'kkk', 500, 1000, 500+140, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'kkk', 500, 1000, 500+150, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'kkk', 500, 1000, 500+180, '2022-07-05');
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'lll', 500, 1000, 500+10, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'lll', 500, 1000, 500+16, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'lll', 500, 1000, 500+123, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'lll', 500, 1000, 500+129, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'lll', 500, 1000, 500+149, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'lll', 500, 1000, 500+159, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'lll', 500, 1000, 500+180, '2022-07-05');
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'mmm', 500, 1000, 500+10, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'mmm', 500, 1000, 500+136, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'mmm', 500, 1000, 500+143, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'mmm', 500, 1000, 500+158, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'mmm', 500, 1000, 500+165, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'mmm', 500, 1000, 500+175, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'mmm', 500, 1000, 500+188, '2022-07-05');
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'nnn', 500, 1000, 500+10, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'nnn', 500, 1000, 500+156, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'nnn', 500, 1000, 500+167, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'nnn', 500, 1000, 500+178, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'nnn', 500, 1000, 500+189, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'nnn', 500, 1000, 500+198, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'nnn', 500, 1000, 500+199, '2022-07-05');
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ooo', 500, 1000, 500+10, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ooo', 500, 1000, 500+56, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ooo', 500, 1000, 500+73, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ooo', 500, 1000, 500+98, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ooo', 500, 1000, 500+145, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ooo', 500, 1000, 500+155, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ooo', 500, 1000, 500+288, '2022-07-05');
-
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ppp', 500, 1000, 500+0, '2022-07-01');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ppp', 500, 1000, 500+5, '2022-07-02');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ppp', 500, 1000, 500+78, '2022-07-03');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ppp', 500, 1000, 500+123, '2022-07-04');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ppp', 500, 1000, 500+157, '2022-07-07');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ppp', 500, 1000, 500+189, '2022-07-06');
-INSERT INTO plotingdb.ranges (status, society, min_limit, max_limit, next_number, snapshot_date) VALUES ('active', 'ppp', 500, 1000, 500+276, '2022-07-05');
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (1, '2022-07-01', 'active', 'aaa', 500, 1000, 500);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (2, '2022-07-01', 'active', 'bbb', 500, 1000, 568);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (3, '2022-07-01', 'active', 'ccc', 500, 1000, 623);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (4, '2022-07-02', 'active', 'aaa', 500, 1000, 520);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (5, '2022-07-02', 'active', 'bbb', 500, 1000, 600);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (6, '2022-07-02', 'active', 'ccc', 500, 1000, 666);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (7, '2022-07-03', 'active', 'aaa', 500, 1000, 540);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (8, '2022-07-03', 'active', 'bbb', 500, 1000, 654);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (9, '2022-07-03', 'active', 'ccc', 500, 1000, 699);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (10, '2022-07-04', 'active', 'aaa', 500, 1000, 560);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (11, '2022-07-04', 'active', 'bbb', 500, 1000, 700);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (12, '2022-07-04', 'active', 'ccc', 500, 1000, 756);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (13, '2022-07-05', 'active', 'aaa', 500, 1000, 588);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (14, '2022-07-05', 'active', 'bbb', 500, 1000, 735);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (15, '2022-07-05', 'active', 'ccc', 500, 1000, 788);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (16, '2022-07-06', 'active', 'aaa', 500, 1000, 610);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (17, '2022-07-06', 'active', 'bbb', 500, 1000, 800);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (18, '2022-07-06', 'active', 'ccc', 500, 1000, 900);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (19, '2022-07-07', 'active', 'aaa', 500, 1000, 630);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (20, '2022-07-07', 'active', 'bbb', 500, 1000, 845);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (21, '2022-07-07', 'active', 'ccc', 500, 1000, 981);
+INSERT INTO plotingdb.ranges (id, snapshot_date, status, society, min_limit, max_limit, next_number) VALUES (22, '2022-07-07', 'active', 'ddd', 500, 1000, 500);
